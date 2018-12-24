f7c5384002c7a266aab585e76f1a182ff8d61e91
Paul Harte <hartepaul@gmail.com>
2018-12-24 18:44:38 +0000

    first commit of Met Eireann widget project

---
+++ app/src/main/java/com/harte/meteireannwidget/ForecastActivity.kt
@@ -0,0 +1,12 @@
+package com.harte.meteireannwidget
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ForecastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+    }
+}
